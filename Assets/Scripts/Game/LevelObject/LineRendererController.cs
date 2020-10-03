fe15389537bbf6aaf9b7fc8d35339ace2e4cbf32
matvei.semerikov <>
2020-10-03 19:54:14 +0300

    Yep

---
+++ Assets/Scripts/Game/LevelObject/LineRendererController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererController : MonoBehaviour
+{
+    public GameObject LineRendererPrefab;
+    
+    public GameObject[] ConnectedPointArray;
+
+    private LineRenderer[] _lineRenderers;
+
+    private void Start()
+    {
+        SpawnLineRenderers();
+
+        DrawLines();
+    }
+
+    private void SpawnLineRenderers()
+    {
+        _lineRenderers = new LineRenderer[ConnectedPointArray.Length];
+
+        for (var i = 0; i < ConnectedPointArray.Length; i++)
+        {
+            var newLr = Instantiate(LineRendererPrefab, gameObject.transform);
+            
+            _lineRenderers[i] = newLr.GetComponent<LineRenderer>();
+        }
+    }
+    public void DrawLines()
+    {
+        for (var i = 0; i < _lineRenderers.Length; i++)
+        {
+            var line = _lineRenderers[i];
+            
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, ConnectedPointArray[i].transform.position);
+        }
+    }
+}
