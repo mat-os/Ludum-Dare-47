ea6ff18bae6cafff44adc7a9ac86c7db8bdead7e
Artem Povaliaev <gevernus@mail.ru>
2020-10-03 15:32:17 +0300

    Добавил префаб транзистора

--- Assets/Scripts/Game/Level/Transistor.cs
+++ Assets/Scripts/Game/Level/Transistor.cs
@@ -1,7 +1,24 @@
+using UnityEngine;
+
 namespace DefaultNamespace
 {
     public class Transistor : Point
     {
-        
+        public GameObject arrows;
+        public float slowKoef;
+
+        public override void BeforeApply(Player player)
+        {
+            base.BeforeApply(player);
+            Time.timeScale /= slowKoef;
+            arrows.SetActive(true);
+        }
+
+        public override void AfterApply(Player player)
+        {
+            base.AfterApply(player);
+            Time.timeScale = 1;
+            arrows.SetActive(false);
+        }
     }
 }
