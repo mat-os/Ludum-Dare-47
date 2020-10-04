a28950b2bdfbaf446561e6cf352d9ce7283e80eb
matvei.semerikov <>
2020-10-04 13:37:47 +0300

    Add redactor
    (cherry picked from commit 6f56abb6605c023f533005a664b69fa886355f64)

---
+++ Assets/Editor/PointRedactorEditor.cs
@@ -0,0 +1,65 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PointRedactor))]
+public class PointRedactorEditor : Editor
+{
+    PointRedactor myTarget;
+    private Point thisPoint;
+
+    public override void OnInspectorGUI()
+    {
+        myTarget = (PointRedactor)target;
+        DrawDefaultInspector();
+        thisPoint = myTarget.GetComponent<Point>();
+        GameObject newPointObject = null;
+        //Линия
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            
+        GUILayout.BeginVertical();
+
+        if (GUILayout.Button("Add Point"))
+        {
+            newPointObject = SpawnObject(PointManager.Instance.PointPrefab);
+            AddConnections(newPointObject);
+        }
+            
+        if (GUILayout.Button("Add Transistor"))
+        {
+             newPointObject = SpawnObject(PointManager.Instance.TransistorPrefab);
+             AddConnections(newPointObject);
+
+        }
+         
+        if (GUILayout.Button("Add Resistor"))
+        {
+             newPointObject = SpawnObject(PointManager.Instance.ResistorPrefab);
+             AddConnections(newPointObject);
+
+        }
+        if (GUILayout.Button("Add Condensator"))
+        {
+             newPointObject = SpawnObject(PointManager.Instance.CondensatorPrefab);
+             AddConnections(newPointObject);
+        }
+        
+        GUILayout.EndVertical();
+
+    }
+
+    private void AddConnections(GameObject newPointObject)
+    {
+        var newPoint = newPointObject.GetComponent<Point>();
+        newPoint._connections.Add(thisPoint);
+        thisPoint._connections.Add(newPoint);
+    }
+
+    private GameObject SpawnObject(GameObject gameObject)
+    {
+        var posToSpawn = myTarget.transform.position + Vector3.up * 1;
+
+        var newPointObject = Instantiate(gameObject, PointManager.Instance.PointHolder.transform);
+        newPointObject.transform.position = posToSpawn;
+        return newPointObject;
+    }
+}
---
+++ Assets/Scripts/PontManager/LineRendererManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class LineRendererManager : MonoBehaviour
+    {
+        public GameObject LineRendererPrefab;
+        private List<LineRenderer> _lineRenderers = new List<LineRenderer>();
+
+        private List<Point> _points = new List<Point>();
+
+        private void Start()
+        {
+            _points =  PointManager.Instance._points;
+            
+            InitLines();
+        }
+
+        private void InitLines()
+        {
+            foreach (var point in _points)
+            {
+                SpawnLineRenderers(point);
+            }
+        }
+        
+        private void SpawnLineRenderers(Point point)
+        {
+            for (var i = 0; i < point._connections.Count; i++)
+            {
+                if (point._connections != null)
+                {
+                    var newLr = Instantiate(LineRendererPrefab, PointManager.Instance.PointHolder.transform).GetComponent<LineRenderer>();
+
+                    newLr.SetPosition(0, point.transform.position);
+                    newLr.SetPosition(1, point._connections[i].transform.position);
+                
+                    _lineRenderers.Add(newLr.GetComponent<LineRenderer>());
+                }
+            }
+        }
+    }
