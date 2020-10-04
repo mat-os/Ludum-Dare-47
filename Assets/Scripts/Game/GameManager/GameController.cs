8b091c8ecaaa2638e3cc611935e6ccf77169e504
Artem Povaliaev <gevernus@mail.ru>
2020-10-04 19:50:49 +0300

    Добавил лого

---
+++ Assets/Scripts/Game/GameManager/GameController.cs
@@ -0,0 +1,55 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.GameManager
+{
+    public class GameController : MonoBehaviourSingleton<GameController>
+    {
+        public Image logo;
+        public Image text;
+        public bool isStarted;
+
+        private Sequence _sequence;
+
+        private void Awake()
+        {
+            _sequence = DOTween.Sequence();
+            _sequence.SetLoops(-1);
+            _sequence
+                .Append(text.DOFade(0, 0.6f))
+                .Append(text.DOFade(1, 0.6f));
+        }
+
+        public void StartGame()
+        {
+            
+            _sequence.Kill();
+            var sequence = DOTween.Sequence();
+            sequence
+                .Append(text.DOFade(0, 0.15f))
+                .Append(text.DOFade(1, 0.15f))
+                .Append(text.DOFade(0, 0.15f))
+                .Append(text.DOFade(1, 0.15f))
+                .Append(text.DOFade(0, 0.15f))
+                .Append(text.DOFade(1, 0.15f))
+                .Append(text.DOFade(0, 0.15f))
+                .Append(text.DOFade(1, 0.15f))
+                .Append(text.DOFade(0, 0.15f));
+            sequence.Play();
+            sequence.OnComplete(() =>
+            {
+                logo.DOFade(0, 0.5f);
+                isStarted = true;
+            });
+        }
+
+        private void Update()
+        {
+            if (!isStarted && Input.anyKey)
+            {
+                StartGame();
+            }
+        }
+    }
+}
